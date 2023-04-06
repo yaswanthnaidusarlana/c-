6f434b6bf3006828f9491ad75948b1ebbeb750b6
yaswanthnaidusarlana <yaswanthyash843@gmai.com>
2023-04-06 16:32:58 +0530

    this is is my first commit

---
+++ repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using excel = Microsoft.Office.Interop.Excel;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            excel.Application xlapp = new excel.Application();
+            excel.Workbook xwb = xlapp.Workbooks.Open("C:\\Users\\Administrator\\Documents\\Book2.xlsx");
+            excel._Worksheet xws = xwb.Sheets[1];
+            excel.Range r1 = xws.UsedRange;
+            Console.WriteLine(r1);
+
+            for (int i = 1; i <= 3; i++)
+            {
+
+                string username, password;
+                username = r1.Cells[1][i].value;
+                password = r1.Cells[3][i].value;
+
+
+
+                Console.WriteLine("hello world");
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://demo.opencart.com/");
+                driver.FindElement(By.LinkText("My Account")).Click();
+                driver.FindElement(By.LinkText("Register")).Click();
+            }
+        }
+    }
+}
+
---
+++ repos/TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace TestProject1
+
+
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
